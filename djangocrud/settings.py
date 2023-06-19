49defb80ab143d0c9418bd16f4af4ade6152c50b
diegods257 <diegoct1855@gmail.com>
2023-06-18 22:34:44 -0500

    sddsdds

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 LOGIN_URL = 'signin'
 
 
@@ -140,7 +140,7 @@ LOGIN_URL = 'signin'
 if not DEBUG:
     # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+    STATIC_ROOT = os.path.join(BASE_DIR, '/static')
 
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
