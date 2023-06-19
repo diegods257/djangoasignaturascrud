ba9697b145cc9e51197652178e1e6283264776ec
diegods257 <diegoct1855@gmail.com>
2023-06-18 22:18:49 -0500

    ewwrere

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'subjects',
 ]
@@ -131,7 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 LOGIN_URL = 'signin'
 
 
