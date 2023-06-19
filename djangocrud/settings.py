52dd439a364478033e0bf775286653450d4ff076
diegods257 <diegoct1855@gmail.com>
2023-06-18 22:03:52 -0500

    dsds

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/subjects/static/'
 LOGIN_URL = 'signin'
 
 
@@ -154,4 +154,3 @@ if not DEBUG:
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'subjects/static'),]
